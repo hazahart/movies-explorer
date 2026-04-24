a98f92e2af60819f60993401460e364eca830adf
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2026-04-23 23:25:38 -0600

    feat: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'netflix-red': '#E50914',
+        'netflix-black': '#141414',
+        'netflix-dark': '#181818',
+        'netflix-gray': '#2F2F2F',
+      },
+    },
   },
   plugins: [],
-}
-
+};
